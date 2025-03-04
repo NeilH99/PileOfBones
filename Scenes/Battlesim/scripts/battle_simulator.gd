32b4f71adde95d56e11b925279cf3f0f0fb5a7a3
NeilH99 <Neilhoran83@gmail.com>
2025-03-04 16:19:22 +0000

    fix btns

--- Scenes/Battlesim/scripts/battle_simulator.gd
+++ Scenes/Battlesim/scripts/battle_simulator.gd
@@ -3,42 +3,43 @@ extends Node2D
 var fightBtnHandler = preload("res://Scenes/Battlesim/scripts/fightBtnHandler.gd").new()
 var myPokemon
 var enemyPokemon
+var home = true
+@onready var btn1 = $CanvasLayer/VBoxContainer/PanelContainer/HomeButtons/Fight
+@onready var btn2 = $CanvasLayer/VBoxContainer/PanelContainer/HomeButtons/Bag
+@onready var btn3 = $CanvasLayer/VBoxContainer/PanelContainer/HomeButtons/Pokemon
+@onready var btn4 = $CanvasLayer/VBoxContainer/PanelContainer/HomeButtons/Run
+@onready var backBtn = $CanvasLayer/VBoxContainer/PanelContainer/HomeButtons/BackBtn
 
 func _ready():
-	# Connect Button1's pressed signal
-	$CanvasLayer/VBoxContainer/PanelContainer/HomeButtons/Fight.pressed.connect(_on_fight_pressed)
+	backBtn.pressed.connect(backBtnPressed)
+	if(home):
+		$CanvasLayer/VBoxContainer/PanelContainer/HomeButtons/Fight.pressed.connect(_on_fight_pressed)
 	generatePokemon()
+	
+
 
 func _on_fight_pressed():
 
-	var btn1 = $CanvasLayer/VBoxContainer/PanelContainer/HomeButtons/Fight
-	var btn2 = $CanvasLayer/VBoxContainer/PanelContainer/HomeButtons/Bag
-	var btn3 = $CanvasLayer/VBoxContainer/PanelContainer/HomeButtons/Pokemon
-	var btn4 = $CanvasLayer/VBoxContainer/PanelContainer/HomeButtons/Run
-
-	btn1.text = myPokemon.moveset[0]
-	btn2.text = "Fight 2 Move"
-	btn3.text = "Fight 3 Move"
-	btn4.text = "Fight 4 Move"
-	
-	var backBtn = btn1.duplicate()
-	backBtn.text = "Back"
-	backBtn.pressed.connect(backBtnPressed.bind(btn1, btn2, btn3, btn4))
-
-	$CanvasLayer/VBoxContainer/PanelContainer/HomeButtons.add_child(backBtn)
-
-	
-	fightBtnHandler.handleFight(btn1, btn2, btn3, btn4)
-	
-func backBtnPressed(fightBtn, bagBtn, pokemonBtn, runBtn):
-	for kid in $CanvasLayer/VBoxContainer/PanelContainer/HomeButtons.get_children():
-		kid.queue_free()
+	if(home):
 		
-	$CanvasLayer/VBoxContainer/PanelContainer/HomeButtons.add_child(fightBtn)
-	$CanvasLayer/VBoxContainer/PanelContainer/HomeButtons.add_child(bagBtn)
-	$CanvasLayer/VBoxContainer/PanelContainer/HomeButtons.add_child(pokemonBtn)
-	$CanvasLayer/VBoxContainer/PanelContainer/HomeButtons.add_child(runBtn)
-	$CanvasLayer/VBoxContainer/PanelContainer/HomeButtons/Fight.pressed.connect(_on_fight_pressed)
+
+		btn1.text = myPokemon.moveset[0]
+		btn2.text = "Fight 2 Move"
+		btn3.text = "Fight 3 Move"
+		btn4.text = "Fight 4 Move"
+		
+		backBtn.visible = true
+		fightBtnHandler.handleFight(btn1, btn2, btn3, btn4)
+		home = false
+	
+func backBtnPressed():
+		
+	btn1.text = "Fight"
+	btn2.text = "Bag"
+	btn3.text = "Pokemon"
+	btn4.text = "Run"
+	home = true
+	backBtn.visible = false
 
 func generatePokemon():
 	var pokemonFolder =  get_files_in_folder("res://Assets/Monsterpool/", "scripts")
